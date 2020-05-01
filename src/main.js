3fc2ef784249bfdfb2f623a0bba2bf5d6dc83d85
djra26111990 <djra26111990@gmail.com>
2020-05-01 00:44:58 -0500

    major changes realtime chat are enable

--- src/main.js
+++ src/main.js
@@ -2,6 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
+
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDXQO8PdFichOWUaIoPVoUyCGm5wNOx0KA",
+  authDomain: "vue-chat-firebase-app-project.firebaseapp.com",
+  databaseURL: "https://vue-chat-firebase-app-project.firebaseio.com",
+  projectId: "vue-chat-firebase-app-project",
+  storageBucket: "vue-chat-firebase-app-project.appspot.com",
+  messagingSenderId: "617468918000"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
+
+window.db = db;
 
 Vue.config.productionTip = false
 
