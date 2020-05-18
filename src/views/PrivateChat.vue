dd923f8fc72b01c43dd26926c05520cc7837d4cc
djra26111990 <djra26111990@gmail.com>
2020-05-18 18:16:33 -0500

    fix scrollHeightproperty of null bug, fix scrolltobottom bug, fix error message sending data to firestore without failing. Major improvement on UI

--- src/views/PrivateChat.vue
+++ src/views/PrivateChat.vue
@@ -1,53 +1,58 @@
 <template>
-<v-container fluid>
-<h3 class=" text-center">Messaging</h3>
-      <div v-for="message in messages" v-bind:key="message.id">
-              <template v-if="message.author === authUser.displayName">
-                <v-chip class="ma-2" label=true color="#90CAF9">
-                  {{ message.message }}
-                </v-chip>
-                <span>{{ message.author }}</span>
-                <v-spacer></v-spacer>
-              </template>
-              <template v-else>
-                <v-chip class="ma-2" label=true color="#A5D6A7">
-                  {{ message.message }}
-                </v-chip>
-                <span>{{ message.author }}</span>
-                <v-spacer></v-spacer>
-              </template>
-            </div>
+<div>
+  <v-container fluid>
+    <div id="container" style="max-height: 410px; overflow-y: auto">
+      <v-list height="90" flat class="overflow-y-auto" v-for="message in messages" v-bind:key="message.id">
+        <template v-if="message.author === authUser.displayName">
+          <v-chip class="ma-2" label color="#90CAF9">
+            {{ message.message }}
+          </v-chip>
+          <span>{{ message.author }}</span>
           <v-spacer></v-spacer>
-          <v-footer fixed=true>
-            <v-row>
-              <v-col
-                cols="12"
-                sm="6"
-              >
-              <v-text-field
-              label="message" 
-              @keyup.enter="saveMessage" 
-              v-model="message"
-              />
-              </v-col>
-              <v-col
-                cols="12"
-                sm="6"
-              >
-              <v-btn @click="saveMessage"
-              fab=true
-              >
-                <v-icon>
-                  mdi-comment-text-outline
-                </v-icon>
-              </v-btn>
-              </v-col>
+        </template>
+        <template v-else>
+          <v-chip class="ma-2" label color="#A5D6A7">
+            {{ message.message }}
+          </v-chip>
+          <span>{{ message.author }}</span>
+          <v-spacer></v-spacer>
+        </template>
+      </v-list>
+    </div>
+  </v-container>
+  <v-spacer></v-spacer>
+  <v-container fluid>
+    <v-footer fixed>
+      <v-row>
+        <v-col
+        cols="12"
+        sm="6"
+        >
+          <v-text-field
+          label="message" 
+          @keyup.enter="saveMessage" 
+          v-model="message"
+          />
+            </v-col>
+            <v-col
+            cols="12"
+            sm="6"
+            >
+            <v-btn @click="saveMessage"
+            fab
+            >
+            <v-icon>
+              mdi-comment-text-outline
+            </v-icon>
+            </v-btn>
+            </v-col>
             </v-row>
             </v-footer>
         <v-btn @click="logout">
           Logout
         </v-btn>
-</v-container>
+        </v-container>
+</div>
 </template>
 
 <script>
@@ -64,9 +69,9 @@ export default {
     }
   },
   methods: {
-    scrollToBottom(){
-      let box = document.querySelector('.msg_history');
-        box.scrollTop = box.scrollHeight;
+    scrollToBottom() {
+       let chatBox = document.querySelector('#container')
+       chatBox.scrollTop = chatBox.scrollHeight
     },
     logout() {
     firebase.auth().signOut().then(() => {
@@ -74,23 +79,22 @@ export default {
     console.log(error)
     });
     },
-    saveMessage() {
+    async saveMessage() {
       //save to firestore
-      db.collection('chat').add({
+      await db.collection('chat').add({
         message: this.message,
         author: this.authUser.displayName,
         createdAt: new Date()
-      }).then(() => {
-        this.scrollToBottom();
       })
-      .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
+      .then((docRef) => {
+         console.log("Document written with ID: ", docRef.id);
       })
-      .catch(function(error) {
-        console.error("Error adding document: ", error);
+      .catch((error) => {
+         console.error("Error adding document: ", error);
       });
       //clean the input msg field
       this.message = null
+      this.scrollToBottom()
     },
     fetchMessages() {
       db.collection('chat').orderBy('createdAt').onSnapshot((querySnapshot) => {
@@ -115,6 +119,10 @@ export default {
     })
 
     this.fetchMessages()
+    this.scrollToBottom()
+  },
+  updated () {
+    this.scrollToBottom()
   },
   beforeRouteEnter(to, from, next) {
     next(vm => {
@@ -131,5 +139,7 @@ export default {
 </script>
 
 <style>
-
-</style>>
+.v-list{
+  overflow-y: auto;
+}
+</style>
